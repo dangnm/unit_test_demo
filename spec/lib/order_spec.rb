6076ff59352d7c95003bb80b453995a1c3a63d6b
Dang Nguyen <minhdang.net@gmail.com>
2018-03-11 15:08:03 +0700

    stub without shared examples

--- spec/lib/order_spec.rb
+++ spec/lib/order_spec.rb
@@ -5,16 +5,16 @@ require_relative '../../lib/purchase'
 require_relative '../../lib/supplier_gateway'
 
 describe Order do
-  # describe '#code' do
-  #   let!(:order) { Order.new }
-  #   it 'returns in correct format' do
-  #     expect(order.respond_to?(:code)).to eq(true)
-  #     expect(order.code.class).to eq(String)
-  #   end
-  # end
-
   describe '#code' do
-    let!(:shared_example_order) { Order.new }
-    it_behaves_like 'order code example'
+    let!(:order) { Order.new }
+    it 'returns in correct format' do
+      expect(order.respond_to?(:code)).to eq(true)
+      expect(order.code.class).to eq(String)
+    end
   end
+
+  # describe '#code' do
+  #   let!(:shared_example_order) { Order.new }
+  #   it_behaves_like 'order code example'
+  # end
 end
--- spec/lib/purchase_spec.rb
+++ spec/lib/purchase_spec.rb
@@ -5,13 +5,13 @@ require_relative '../../lib/purchase'
 require_relative '../../lib/supplier_gateway'
 
 describe Purchase do
-  describe 'parse correct auth data (mock example)' do
-    let!(:order) { Order.new }
-    it 'should send purchase message to the gateway' do
-      expect_any_instance_of(SupplierGateway).to receive(:purchase)
-      expect(Purchase.new(order).make_purchase).to eq(true)
-    end
-  end
+  # describe 'parse correct auth data (mock example)' do
+  #   let!(:order) { Order.new }
+  #   it 'should send purchase message to the gateway' do
+  #     expect_any_instance_of(SupplierGateway).to receive(:purchase)
+  #     expect(Purchase.new(order).make_purchase).to eq(true)
+  #   end
+  # end
 
   describe 'parse correct auth data (stub example 1)' do
     let!(:order) {
@@ -50,16 +50,16 @@ describe Purchase do
     end
   end
 
-  describe 'parse correct auth data (stub example 4)' do
-    let!(:shared_example_order) {
-      double("OrderTest", :code => "AAA")
-    }
+  # describe 'parse correct auth data (stub example 4)' do
+  #   let!(:shared_example_order) {
+  #     double("OrderTest", :code => "AAA")
+  #   }
 
-    it_behaves_like 'order code example'
+  #   it_behaves_like 'order code example'
 
-    it 'should send purchase message to the gateway' do
-      expect_any_instance_of(SupplierGateway).to receive(:purchase)
-      expect(Purchase.new(shared_example_order).make_purchase).to eq(true)
-    end
-  end
+  #   it 'should send purchase message to the gateway' do
+  #     expect_any_instance_of(SupplierGateway).to receive(:purchase)
+  #     expect(Purchase.new(shared_example_order).make_purchase).to eq(true)
+  #   end
+  # end
 end
